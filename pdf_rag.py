d28c6c706e9c55be3d5cf7f91303c3f35abc623d
Bhavyesh Joshi <37145401+bhavyeshjoshipersonal@users.noreply.github.com>
2025-06-28 00:14:28 +0000

    Initial RAG working
    Initial RAG working

--- pdf_rag.py
+++ pdf_rag.py
@@ -4,6 +4,11 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
 import ollama
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import ChatOllama
+from langchain_core.runnables import RunnablePassthrough
+from langchain.retrievers.multi_query import MultiQueryRetriever
 
 doc_path = r"C:\Rag_App\data\tsla-20241023-gen.pdf"
 #doc_path = r"C:\Rag_App\BOI.pdf"
@@ -50,3 +55,37 @@ vectorstore = Chroma.from_documents(
 print("Embeddings added to Chroma vector store.")
 
 #Retrieve documents based on a query
+llm = ChatOllama(model=model)
+
+QUERY_PROMPT = PromptTemplate(
+    input_variables=["question"],
+    template="""You are an AI language model assistant. Your task is to generate five
+    different versions of the given user question to retrieve relevant documents from
+    a vector database. By generating multiple perspectives on the user question, your
+    goal is to help the user overcome some of the limitations of the distance-based
+    similarity search. Provide these alternative questions separated by newlines.
+    Original question: {question}""",
+)
+
+retriever = MultiQueryRetriever.from_llm(
+    vectorstore.as_retriever(),
+    llm,
+    prompt=QUERY_PROMPT)
+
+# RAG prompt
+template = """Answer the question based ONLY on the following context:
+{context}
+Question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+res = chain.invoke(input=("what is the document about?",))
+print(f"Response: {res}")
